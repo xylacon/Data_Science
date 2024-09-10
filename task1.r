e14aba9fce0e78368f867ef2b62e3f90a41a4ac6
xylacon <65502587+xylacon@users.noreply.github.com>
2024-09-10 12:31:40 -0500

    Fixed subtask 1

--- task1.r
+++ task1.r
@@ -1,8 +1,19 @@
 data_set <- read.csv("Baseball_Databank_Teams_1871_2023_Modded.csv")
 
 # 1. Covariance matrices
-cov_matrix <- cov(data_set[, c("R", "E", "HR", "RA", "SOA")])
-print(cov_matrix)
+# cov_matrix <- cov(data_set[, c("R", "E", "HR", "RA", "SOA")])
+# print(cov_matrix)
+
+cov_pairs <- list(c("R", "E"), c("R", "HR"), c("R", "RA"), c("R", "SOA"), c("E", "HR"), c("E", "RA"), c("E", "SOA"), c("HR", "RA"), c("HR", "SOA"), c("RA", "SOA"))
+covariances <- list()
+cat("Covariance matrix pairs:\n")
+for (i in seq_along(cov_pairs)) {
+	pair <- cov_pairs[[i]]
+	covariances[[i]] <- cov(data_set[, pair])
+
+	print(covariances[[i]])
+	cat("\n")
+}
 
 # 2. Scatter plots
 plot(data_set[["AB"]], data_set[["H"]],
