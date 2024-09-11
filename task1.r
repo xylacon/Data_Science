cb5bad7f510e352a2ab165a30bd0077af782784d
xylacon <65502587+xylacon@users.noreply.github.com>
2024-09-10 21:57:43 -0500

    Updated subtask 1

--- task1.r
+++ task1.r
@@ -4,14 +4,20 @@ data_set <- read.csv("Baseball_Databank_Teams_1871_2023_Modded.csv")
 # cov_matrix <- cov(data_set[, c("R", "E", "HR", "RA", "SOA")])
 # print(cov_matrix)
 
-cov_pairs <- list(c("R", "E"), c("R", "HR"), c("R", "RA"), c("R", "SOA"), c("E", "HR"), c("E", "RA"), c("E", "SOA"), c("HR", "RA"), c("HR", "SOA"), c("RA", "SOA"))
+pairs <- list(c("R", "E"), c("R", "HR"), c("R", "RA"), c("R", "SOA"), c("E", "HR"), c("E", "RA"), c("E", "SOA"), c("HR", "RA"), c("HR", "SOA"), c("RA", "SOA"))
 covariances <- list()
+correlations <- list()
+
 cat("Covariance matrix pairs:\n")
-for (i in seq_along(cov_pairs)) {
-	pair <- cov_pairs[[i]]
+for (i in seq_along(pairs)) {
+	pair <- pairs[[i]]
 	covariances[[i]] <- cov(data_set[, pair])
+	correlations[[i]] <- cor(data_set[, pair])
 
 	print(covariances[[i]])
+	cat("\n")
+	cat("Correlation: ")
+	print(correlations[[i]])
 	cat("\n")
 }
 
