4043ec96409d66f9ee792bddb7ddaa99ee7e3c86
xylacon <65502587+xylacon@users.noreply.github.com>
2024-09-12 23:36:08 -0500

    Completed subtask 3

--- task1.r
+++ task1.r
@@ -1,36 +1,42 @@
 data_set <- read.csv("Baseball_Databank_Teams_1871_2023_Modded.csv")
 
 # 1. Covariance matrices
-# cov_matrix <- cov(data_set[, c("R", "E", "HR", "RA", "SOA")])
-# print(cov_matrix)
-
 pairs <- list(c("R", "E"), c("R", "HR"), c("R", "RA"), c("R", "SOA"), c("E", "HR"), c("E", "RA"), c("E", "SOA"), c("HR", "RA"), c("HR", "SOA"), c("RA", "SOA"))
 covariances <- list()
 correlations <- list()
 
-cat("Covariance matrix pairs:\n")
 for (i in seq_along(pairs)) {
 	pair <- pairs[[i]]
 	covariances[[i]] <- cov(data_set[, pair])
 	correlations[[i]] <- cor(data_set[, pair])
 
+	cat("Covariance:\n")
 	print(covariances[[i]])
-	cat("\n")
-	cat("Correlation: ")
+	cat("\nCorrelation:\n")
 	print(correlations[[i]])
-	cat("\n")
+	cat("\n\n")
 }
 
 # 2. Scatter plots
-plot(data_set[["AB"]], data_set[["H"]],
-	xlab="At Bats",
-	ylab="Hits By Batters")
-plot(data_set[["HA"]], data_set[["BBA"]],
-	xlab="Hits Allowed",
-	ylab="Walks Allowed")
+plot(data_set$AB, data_set$H,
+	xlab = "At Bats",
+	ylab = "Hits By Batters")
+plot(data_set$HA, data_set$BBA,
+	xlab = "Hits Allowed",
+	ylab = "Walks Allowed")
 
 # 3. Histograms
+teams <- c("Houston Astros", "Seattle Mariners", "New York Yankees")
 
+for (team in teams) {
+	set_1 <- subset(data_set, name == team & yearID >= 2004 & yearID <= 2013 & TARGET %in% c("HIGH", "AVERAGE", "LOW"))
+	set_2 <- subset(data_set, name == team & yearID >= 2014 & yearID <= 2023 & TARGET %in% c("HIGH", "AVERAGE", "LOW"))
+
+	barplot(table(set_1$TARGET),
+		main = paste(team, "(2004-2013)"))
+	barplot(table(set_2$TARGET),
+		main = paste(team, "(2014-2023)"))
+}
 
 # 4. Box plots
 
